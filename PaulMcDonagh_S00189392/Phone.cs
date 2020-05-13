31eea24bdc775679d815fbdb8d5559042677e9f0
Paul Mc Donagh <S00189392@mail.itsligo.ie>
2020-05-13 10:04:56 +0100

    Added the Phone class

---
+++ PaulMcDonagh_S00189392/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulMcDonagh_S00189392
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Operating_System { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+    }
+}
